40909be2d9efb0e3f124817b9f6528b10fa78d02
xiaofan <zeng-dream@live.com>
2016-09-03 19:01:21 +0800

    数据提取——多线程

--- src/privateUtil/DataExtractUtil.java
+++ src/privateUtil/DataExtractUtil.java
@@ -7,10 +7,12 @@ import java.io.OutputStreamWriter;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.regex.Matcher;
@@ -33,20 +35,20 @@ import com.google.gson.Gson;
  * @author zxiaofan
  */
 public class DataExtractUtil {
-    private static String databaseName = "ComptPriceOrderOperLog"; // 基础数据库名
+    static String databaseName = "ComptPriceOrderOperLog"; // 基础数据库名
 
-    private static String tableNameBasic = "OrderOperLog"; // 基础表名
+    static String tableNameBasic = "OrderOperLog"; // 基础表名
 
-    private static String timeStart = "20160816"; // 按年分表只需填写年份,按天分表需年月日格式
+    private static String timeStart = "20160801"; // 按年分表只需填写年份,按天分表需年月日格式
 
-    private static String timeEnd = "20160817";
+    private static String timeEnd = "20160831";
 
-    private static int timeout = 9000; // 超时设置
+    static int timeout = 9000; // 超时设置
 
-    private static int autoChangeTableName = 1; // 自动更换基础表名tableName模式（1自动，2手动）
+    static int autoChangeTableName = 1; // 自动更换基础表名tableName模式（1自动，2手动）
 
     // sql中的所有表名请用dynamicTableName代替(手动模式)
-    private static String sql = "SELECT A1,A2 FROM OrderOperLog20160816 WHERE A8 LIKE '%供应资源换编码失败%有外放舱位%' AND A1 in (SELECT DISTINCT A1 FROM OrderOperLog WHERE A8 like '%新供应商：内部采购%' AND A1 in(SELECT A1 FROM OrderOperLog WHERE A8 like '%同定同出外放无舱位%' OR A8 LIKE '%订单没有换编码—zd%' OR A8 LIKE '%换编码外放无舱位%' OR PNRContent like '%同定同出外放无舱位%' OR PNRContent like '%订单没有换编码—zd%' OR PNRContent like '%换编码外放无舱位%'));";
+    static String sql = "查询sql语句";
 
     public static void main(String[] args) throws Exception {
         DataExtractUtil.start();
@@ -57,11 +59,20 @@ public class DataExtractUtil {
             System.out.println("sql中的所有表名请用dynamicTableName代替");
         }
         System.out.println("数据提取ing...");
+        pathSave = pathSave + formatd.format(new Date()) + "\\";
         List<String> tableList = getAllTableName(timeStart, timeEnd);
         createFile(pathSave);
-        StringBuffer allData = new StringBuffer();
         for (String tableName : tableList) {
-            querySave(tableName, allData);
+            service.submit(new queryDataThread(tableName));
+            // querySave(tableName, allData);
+        }
+        service.shutdown();
+        while (!service.isTerminated()) {
+            Thread.sleep(3000);
+        }
+        StringBuffer allData = new StringBuffer();
+        for (String str : mapAllData.values()) {
+            allData.append(str);
         }
         fileSave(allData.toString(), pathSave + tableNameBasic + timeStart + "-" + timeEnd + ".txt");
         System.out.println("数据提取End...");
@@ -124,39 +135,6 @@ public class DataExtractUtil {
         return tableList;
     }
 
-    public static void querySave(String tableName, StringBuffer allData) throws Exception {
-        Map<String, String> queryMap = new HashMap<String, String>();
-        String querySql = sql;
-        queryVo vo = new queryVo();
-        vo.setDatabaseName(databaseName);
-        if (2 == autoChangeTableName) { // 手动模式
-            querySql = querySql.replaceAll("dynamicTableName", tableName);
-        } else if (1 == autoChangeTableName) { // 自动更换基础表名
-            String regex = "\\s[fromFROM]{4}\\s" + tableNameBasic + "[0-9\\-_]*?\\s";
-            Pattern pattern = Pattern.compile(regex);
-            Matcher matcher = pattern.matcher(querySql);
-            while (matcher.find()) {
-                querySql = matcher.replaceAll(" FROM " + tableName + " ");
-            }
-        }
-        // querySql.replaceAll("(\r\n|\r|\n|\n\r)", "<br>");
-        querySql.replaceAll("\\u0027", "'");
-        vo.setSql(querySql);
-        queryMap.put("data", gson.toJson(vo));
-        Document doc = jsoupDoc(url, queryMap, Method.POST);
-        Map<String, String> map = gson.fromJson(doc.text(), Map.class);
-        if ("2".equals(map.get("code"))) {
-            System.out.println(tableName + "_异常");
-            System.out.println(map.get("msg"));
-            listErrTable.add(tableName);
-            return;
-        }
-        Document docRes = jsoupDoc(referer + map.get("msg"), queryMap, Method.GET);
-        String result = keepLineBreak(docRes);
-        allData.append(result);
-        fileSave(result, pathSave + tableName + ".txt");
-    }
-
     /**
      * 保留换行符.
      */
@@ -169,31 +147,7 @@ public class DataExtractUtil {
         return result;
     }
 
-    public static Document jsoupDoc(String url, Map<String, String> map, Method method) throws Exception {
-        Connection connection = Jsoup.connect(url);
-        connection.timeout(timeout); // 设置连接超时时间
-        // 给服务器发消息头，告诉服务器，俺不是java程序。
-        connection.header("User-Agent", "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0)");
-        connection.header("Accept", "application/json, text/javascript, */*; q=0.01");
-        connection.header("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-        connection.header("Host", "172.16.7.21:4385");
-        connection.header("Accept-Encoding", "gzip, deflate");
-        Response doc = null;
-        try {
-            if (Method.POST.equals(method)) {
-                connection.post();
-            } else {
-                connection.get();
-            }
-            doc = connection.ignoreContentType(true).method(method).data(map).execute();
-        } catch (IOException e) {
-            System.err.println(url);
-            e.printStackTrace();
-        } // 获取返回的html的document对象
-        return doc.parse();
-    }
-
-    private static void fileSave(String param, String path) {
+    static void fileSave(String param, String path) {
         // 输出到本地文本
         createFile(path);
         File file = new File(path);
@@ -231,11 +185,11 @@ public class DataExtractUtil {
         }
     }
 
-    private static String pathSave = "d:\\dataSave\\"; // 保存位置
+    static String pathSave = "d:\\dataSave\\"; // 保存位置
 
-    private static String url = "http://172.16.7.21:4385/Database/queryData";
+    static String url = "http://172.16.7.21:4385/Database/queryData";
 
-    private static String referer = "http://172.16.7.21:4385/";
+    static String referer = "http://172.16.7.21:4385/";
 
     private static SimpleDateFormat formaty = new SimpleDateFormat("yyyy");
 
@@ -243,12 +197,16 @@ public class DataExtractUtil {
 
     private static SimpleDateFormat formatd = new SimpleDateFormat("yyyyMMdd");
 
-    private static List<String> listErrTable = new ArrayList<>(); // 为提取到数据的表
+    static List<String> listErrTable = Collections.synchronizedList(new ArrayList<>()); // 未提取到数据的表
 
     private static ExecutorService service = Executors.newFixedThreadPool(10);
 
+    static ConcurrentHashMap<String, String> mapAllData = new ConcurrentHashMap<>(); // 所有数据集
+
     // private static Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create(); // 保留特殊字符
-    private static Gson gson = new Gson();
+    static Gson gson = new Gson();
+
+    public static String tableName;
 
 }
 
@@ -323,4 +281,80 @@ class queryVo {
         this.isRead = isRead;
     }
 
+}
+
+class queryDataThread implements Runnable {
+    private String tableName;
+
+    public queryDataThread(String tableName) {
+        super();
+        this.tableName = tableName;
+    }
+
+    /**
+     * {@inheritDoc}.
+     */
+    @Override
+    public void run() {
+        Map<String, String> queryMap = new HashMap<String, String>();
+        String querySql = DataExtractUtil.sql;
+        queryVo vo = new queryVo();
+        vo.setDatabaseName(DataExtractUtil.databaseName);
+        if (2 == DataExtractUtil.autoChangeTableName) { // 手动模式
+            querySql = querySql.replaceAll("dynamicTableName", tableName);
+        } else if (1 == DataExtractUtil.autoChangeTableName) { // 自动更换基础表名
+            String regex = "\\s[fromFROM]{4}\\s" + DataExtractUtil.tableNameBasic + "[0-9\\-_]*?\\s";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(querySql);
+            while (matcher.find()) {
+                querySql = matcher.replaceAll(" FROM " + tableName + " ");
+            }
+        }
+        // querySql.replaceAll("(\r\n|\r|\n|\n\r)", "<br>");
+        querySql.replaceAll("\\u0027", "'");
+        vo.setSql(querySql);
+        queryMap.put("data", DataExtractUtil.gson.toJson(vo));
+        Document doc;
+        try {
+            doc = jsoupDoc(DataExtractUtil.url, queryMap, Method.POST);
+            Map<String, String> map = DataExtractUtil.gson.fromJson(doc.text(), Map.class);
+            if ("2".equals(map.get("code"))) {
+                System.out.println(tableName + "_异常");
+                // System.out.println(map.get("msg"));
+                DataExtractUtil.listErrTable.add(tableName);
+                return;
+            }
+            Document docRes = jsoupDoc(DataExtractUtil.referer + map.get("msg"), queryMap, Method.GET);
+            String result = DataExtractUtil.keepLineBreak(docRes);
+            // allData.append(result);
+            DataExtractUtil.mapAllData.put(tableName, result);
+            DataExtractUtil.fileSave(result, DataExtractUtil.pathSave + tableName + ".txt");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Document jsoupDoc(String url, Map<String, String> map, Method method) throws Exception {
+        Connection connection = Jsoup.connect(url);
+        connection.timeout(DataExtractUtil.timeout); // 设置连接超时时间
+        // 给服务器发消息头，告诉服务器，俺不是java程序。
+        connection.header("User-Agent", "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0)");
+        connection.header("Accept", "application/json, text/javascript, */*; q=0.01");
+        connection.header("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+        connection.header("Host", "172.16.7.21:4385");
+        connection.header("Accept-Encoding", "gzip, deflate");
+        Response doc = null;
+        try {
+            if (Method.POST.equals(method)) {
+                connection.post();
+            } else {
+                connection.get();
+            }
+            doc = connection.ignoreContentType(true).method(method).data(map).execute();
+        } catch (IOException e) {
+            System.err.println(url);
+            // e.printStackTrace();
+        } // 获取返回的html的document对象
+        return doc.parse();
+    }
 }
