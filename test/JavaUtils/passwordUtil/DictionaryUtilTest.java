2a633cb9812dcd25ba455355a2fc68908f0e2678
zxiaofan <zeng-dream@live.com>
2017-02-18 20:17:36 +0800

    DictionaryUtil：新增校验器，支持生成字典实时破解7Z压缩包密码

---
+++ test/JavaUtils/passwordUtil/DictionaryUtilTest.java
@@ -0,0 +1,68 @@
+/*
+ * 文件名：DictionaryUtilTest.java
+ * 版权：Copyright 2007-2017 517na Tech. Co. Ltd. All Rights Reserved. 
+ * 描述： DictionaryUtilTest.java
+ * 修改人：xiaofan
+ * 修改时间：2017年2月18日
+ * 修改内容：新增
+ */
+package passwordUtil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import utils.ZipPwdUtil;
+
+/**
+ * 
+ * @author xiaofan
+ */
+public class DictionaryUtilTest {
+    static String path7ZFile = "E:\\123.7z";
+
+    static int lengthMin = 5;
+
+    static int lengthMax = 8;
+
+    List<String> param = new ArrayList<>(Arrays.asList("z", "x", "f", "c"));
+
+    /**
+     * 校验器myCheck实时校验.
+     */
+    @Test
+    public void testValidate() {
+        for (int i = 0; i < 10; i++) {
+            param.add(i + "");
+        }
+        DictionaryUtil.validate(param, lengthMin, lengthMax, myCheck);
+    }
+
+    /**
+     * 生成全量字典.
+     * 
+     */
+    @Test
+    public void testBuildAll() {
+        List<String> result = DictionaryUtil.buildAll(param, 3, 3);
+        System.out.println(result.size() + ":" + result.toString());
+    }
+
+    /**
+     * 重写校验器，校验7z压缩包密码.
+     */
+    static Check myCheck = new Check(lengthMin, lengthMax) {
+        /**
+         * {@inheritDoc}.
+         */
+        @Override
+        boolean execute(String param) {
+            if (ZipPwdUtil.validatePwd(path7ZFile, param)) {
+                return true;
+            }
+            return false;
+        }
+    };
+}
